d2d66ca7c050e2b9be5f6f204fea8f21fb348896
Daniel Hüsch <daniel.huesch@gmail.com>
2011-04-18 10:52:06 +0200

    Show stack versions on root.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def index
+    render :text => <<-EO_INFO
+      Ruby: #{RUBY_VERSION rescue 'n/a'}
+      Rubygems: #{Gem::VERSION rescue 'n/a'}
+      Bundler: #{Bundler::VERSION rescue 'n/a'}
+      Passenger: #{PhusionPassenger::VERSION_STRING rescue 'n/a'}
+    EO_INFO
+  end
 end
