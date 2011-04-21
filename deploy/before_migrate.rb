18e8ee16920fe7352bfe6341764fc898def905d7
Daniel Hüsch <daniel.huesch@gmail.com>
2011-04-21 13:55:59 +0200

    Use template resource from within deploy hook.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,12 @@
+template '/root/scalarium-agent/some_file' do
+  cookbook 'agent_version'
+  source 'TARGET_VERSION.erb'
+  owner 'root'
+  group 'root'
+  mode 0600
+  variables :version => node.scalarium.agent_version
+end
+
 ::File.open("/tmp/dhdbg-#{__FILE__.gsub('/', '_')}", 'w') do |file|
   file.puts "#{release_path.sub(/releases\/\d+/, 'shared')}/config/database.yml='#{`cat #{release_path.sub(/releases\/\d+/, 'shared')}/config/database.yml 2>&1`}'"
   file.puts "#{release_path}/config/database.yml='#{`cat #{release_path}/config/database.yml 2>&1`}'"
